eb94311258f042980e67785272fd79a94f64d38b
Zheng Meyer-Zhao <zheng.meyer@gmail.com>
2015-08-24 09:20:07 +0800

    Remove extra comments accidentally added

--- src/datasim.h
+++ src/datasim.h
@@ -32,23 +32,8 @@
   #define ISCOMPLEX 0
   #define BITSPERBYTE 8
 
-  #define SEFD 1000/*****************************************************************************
-*    <DataSim: VLBI data simulator>                                          * 
-*    Copyright (C) <2015> <Zheng Meyer-Zhao>                                 *
-*                                                                            *
-*    This program is free software: you can redistribute it and/or modify    *
-*    it under the terms of the GNU General Public License as published by    *
-*    the Free Software Foundation, either version 3 of the License, or       *
-*    (at your option) any later version.                                     *
-*                                                                            *
-*    This program is distributed in the hope that it will be useful,         *
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
-*    GNU General Public License for more details.                            *
-*                                                                            *
-*    You should have received a copy of the GNU General Public License       *
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
-*****************************************************************************/#endif /* __DATASIM_H__ */
+  #define SEFD 1000
+  #endif /* __DATASIM_H__ */
 
 /*
  * eof
